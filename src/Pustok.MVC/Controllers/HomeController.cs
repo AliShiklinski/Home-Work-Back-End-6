3c6a8c93a9b2c8fdf608b407f05767e9b145efaf
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-09-06 22:19:40 +0400

    added

---
+++ src/Pustok.MVC/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Pustok.Business.Services.Implementations;
+using Pustok.Business.Services.Interfaces;
+using Pustok.MVC.ViewModels;
+using System.Diagnostics;
+
+namespace Pustok.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IGenreService _genreService;
+        private readonly IBookService bookService;
+        private readonly ISlideService slideService;
+
+        public HomeController(IGenreService genreService, IBookService bookService, ISlideService slideService)
+        {
+            _genreService = genreService;
+            this.bookService = bookService;
+            this.slideService = slideService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var featuredBooks = await bookService.GetAllAsync(x => !x.IsDeleted, "BookImages", "Author", "Genre");
+            var newArrivals = await bookService.GetAllByOrderAsync(x => !x.IsDeleted, b => b.CreatedAt, "BookImages", "Author", "Genre");
+
+            var mostExpensiveBooks = await bookService.GetAllByOrderAsync(x => !x.IsDeleted, b => b.SalePrice, "BookImages", "Author", "Genre");
+            var slides = await slideService.GetAllAsync(); 
+
+            var model = new HomeVM
+            {
+                FeaturedBooks = featuredBooks,
+                NewArrivals = newArrivals,
+                MostExpensiveBooks = mostExpensiveBooks,
+                Slides = slides
+            };
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> AddToBasket(int? bookId)
+        {
+            if (bookId < 1 || bookId is null)
+            {
+                return NotFound();
+            }
+
+            if(await bookService.IsExist(x => x.Id == bookId) == false)
+            {
+                return NotFound();
+            }
+
+
+            List<BasketItemVM> basketItems = new List<BasketItemVM>();
+            BasketItemVM basketItem = null;
+            string existedItems = HttpContext.Request.Cookies["BasketItems"];
+            if (existedItems != null)
+            {
+                basketItems = JsonConvert.DeserializeObject<List<BasketItemVM>>(existedItems);
+
+                basketItem = basketItems.FirstOrDefault(x => x.BookId == bookId);
+                if (basketItem != null)
+                {
+                    basketItem.Count++;
+                    
+                }
+                else
+                {
+                    basketItem = new BasketItemVM
+                    {
+                        BookId = bookId,
+                        Count = 1
+                    };
+                    basketItems.Add(basketItem);
+                }
+            }
+            else
+            {
+                basketItem = new BasketItemVM
+                {
+                    BookId = bookId,
+                    Count = 1
+                };
+                basketItems.Add(basketItem);
+            }
+           
+            
+            string basketItemsStr = JsonConvert.SerializeObject(basketItems);
+
+            HttpContext.Response.Cookies.Append("BasketItems", basketItemsStr);
+
+            return Ok();
+
+        }
+
+
+        public IActionResult GetBasketItems()
+        {
+            string data = HttpContext.Request.Cookies["BasketItems"];
+            List<BasketItemVM> basketItems = new List<BasketItemVM>();
+            if (data != null)
+            {
+                basketItems = JsonConvert.DeserializeObject<List<BasketItemVM>>(data);
+            }
+
+
+            return Ok(basketItems);
+        }
+
+        public IActionResult BasketUpdate()
+        {
+            return ViewComponent("Basket");
+        }
+    }
+}
